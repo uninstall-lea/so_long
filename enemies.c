27aff5b2e7ef7652d72d9761a9ad8a104bff148b
uninstall-lea <lea.bisson@gmail.com>
2022-05-23 21:37:05 +0200

    clean

--- enemies.c
+++
@@ -1,4 +0,0 @@
-/* Voir où se trouve le char ENEMIES */
-/* Check si quelque chose peut entraver sa patrouille */
-/* le faire patrouiller de son spawn au prochain obstacle "1, C, E" */
-/* le faire patrouiller dans l'autre sens jusqu'à son spwan */
